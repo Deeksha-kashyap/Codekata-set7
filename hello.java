da31291ae02d1a98692cac07167bfa480f80cb3b
Deeksha-kashyap <47790525+Deeksha-kashyap@users.noreply.github.com>
2019-02-26 01:23:25 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class JavaProgram
+{
+   public static void main(String[] args)
+   {
+      int n;
+      System.out.print("Enter the number");
+      Scanner scan= new Scanner(System.in);
+      n=scan.nextInt();
+      for(int i=0;i<n;i++)
+      {
+          System.out.println("Hello");
+      }
+   }
+}
